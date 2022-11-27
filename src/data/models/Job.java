e4712f9696fff8a1c817aacaffa270f33f5ca649
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-11-28 00:36:51 +0600

    Define models

---
+++ src/data/models/Job.java
@@ -0,0 +1,6 @@
+package data.models;
+
+public class Job {
+    public int id;
+    public String name;
+}
---
+++ src/data/models/User.java
@@ -0,0 +1,11 @@
+package data.models;
+
+import java.sql.Date;
+
+public class User {
+    public int id;
+    public String name;
+    public int job_id;
+    public String job;
+    public Date BirthDate;
+}
